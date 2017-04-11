3082ee416e1e0c40e03ed73aa7b2795bca7eec02
davic-cit <davic@ciandt.com>
2017-04-11 10:30:50 -0300

    Test for travis

---
+++ travis.sh
@@ -0,0 +1,22 @@
+#!/bin/bash
+set -e
+
+PATHS="
+drupal_module/phantomsheet
+:!*.less
+:!*.png
+:!*.json
+:!*.csv
+"
+
+CHANGED_FILES="$(git diff --name-only --diff-filter=d origin/master -- $PATHS)"
+
+echo $CHANGED_FILES
+
+if [[ -n "${CHANGED_FILES// }" ]]; then
+  echo "$CHANGED_FILES" | xargs phpcs -v --standard=Drupal,DrupalPractice
+
+  for FILE in $CHANGED_FILES; do
+    php -l $FILE
+  done
+fi
